77a1b6779b20fc2303a0224727b37cbcc0bdd328
FalguniTanna <falgunitanna01@gmail.com>
2024-08-10 13:50:23 +0530

    cropModel

---
+++ server/model/cropModel.js
@@ -0,0 +1,36 @@
+const cropSchema = new mongoose.Schema({
+    cropType: {
+      type: String,
+      required: true,
+    },
+    variety: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    priceExpectation: {
+      type: Number,
+    },
+    harvestDate: {
+      type: Date,
+      required: true,
+    },
+    deliveryWindow: {
+      type: String,
+      required: true,
+    },
+    farmerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Farmer',
+      required: true,
+    },
+  }, { timestamps: true });
+  
+  module.exports = mongoose.model('Crop', cropSchema);
+  
